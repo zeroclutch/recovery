3e813a2f8123edd0be046a9832133a1e7308c418
P G <35671110+zeroclutch@users.noreply.github.com>
2021-03-27 15:25:22 -0700

    Add landing page

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -3,18 +3,37 @@ import React from 'react'
 import '../../styles/bulma.sass'
 import './LandingPage.scss'
 import { Link } from 'react-router-dom'
-import ConfirmAddItem from "../../components/ConfirmAddItem/ConfirmAddItem";
+import AddToHomeScreen from '@ideasio/add-to-homescreen-react';
+
 
 
 function LandingPage(props) {
   return (
     <div className="LandingPage has-text-centered">
       <div class="header-hero">
-        <h1 class="main-title">
-          <span class="glass">re</span>
-          <span class="glow">cov</span>
-          <span class="glass">ery</span>
-        </h1>
+        <div class="header-content">
+          <h1 class="main-title">
+            <span class="glass">re</span>
+            <span class="glow">cov</span>
+            <span class="glass">ery</span>
+          </h1>
+          <h2 class="subheading glass p-4">the food app for recovering COVID&#8209;19 patients</h2>
+          <h2 class="text-style-header-5 has-text-white p-4">Add this webapp to your home screen to use it.</h2>
+        
+              <AddToHomeScreen
+              autostart={ true }
+              customPromptContent={ {
+                title: 'Do you want to install Recovery on your homescreen?',
+                cancelMsg: 'No',
+                installMsg: 'Yes, sure!',
+                guidanceCancelMsg: 'Dismiss',
+              }
+              }/>
+          <div class="main-content p-4">
+            <div class="main-content-holder">
+            </div>
+          </div>
+      </div>
       </div>
     </div>
   )
